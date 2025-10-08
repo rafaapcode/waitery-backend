a8a2c995fd38388e5a01a16bf2c7b79e27700e41
rafaapcode <rafaap2003@gmail.com>
2025-10-07 21:56:13 -0300

    tests: testing the getAllOrders usecase

--- src/modules/order/tests/usecase/getMy-orders.usecase.spec.ts
+++ src/modules/order/tests/usecase/getMy-orders.usecase.spec.ts
@@ -1,9 +1,7 @@
-import { NotFoundException } from '@nestjs/common';
 import { Test, TestingModule } from '@nestjs/testing';
 import { Prisma } from 'generated/prisma';
 import { IOrderContract } from 'src/core/application/contracts/order/IOrderContract';
 import { IOrganizationContract } from 'src/core/application/contracts/organization/IOrganizationContract';
-import { Order } from 'src/core/domain/entities/order';
 import { UserRole } from 'src/core/domain/entities/user';
 import { PrismaService } from 'src/infra/database/database.service';
 import { OrganizationService } from 'src/modules/organization/organization.service';
@@ -22,6 +20,7 @@ describe('Get All Orders UseCase', () => {
   let prismaService: PrismaService;
   let org_id: string;
   let user_id: string;
+  let user_id2: string;
 
   beforeAll(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -61,6 +60,17 @@ describe('Get All Orders UseCase', () => {
       },
     });
 
+    const user2 = await prismaService.user.create({
+      data: {
+        cpf: '22222222222',
+        name: 'rafael ap',
+        email: 'rafaap321321321@gmail.com',
+        password:
+          '$2a$12$e18NpJDNs7DmMRkomNrvBeo2GiYNNKnaALVPkeBFWu2wALkIVvf.u', // qweasdzxc2003
+        role: UserRole.ADMIN,
+      },
+    });
+
     const org = await prismaService.organization.create({
       data: {
         name: 'Restaurante Fogo de chão',
@@ -87,19 +97,21 @@ describe('Get All Orders UseCase', () => {
         table: `Mesa ${idx}`,
         total_price: 120,
         org_id: org.id,
-        user_id: user.id,
+        user_id: user2.id,
         products: [] as Prisma.JsonArray,
       })),
     });
 
     org_id = org.id;
     user_id = user.id;
+    user_id2 = user2.id;
   });
 
   afterAll(async () => {
     await prismaService.order.deleteMany({ where: { org_id: org_id } });
     await prismaService.organization.delete({ where: { id: org_id } });
     await prismaService.user.delete({ where: { id: user_id } });
+    await prismaService.user.delete({ where: { id: user_id2 } });
   });
 
   it('Should all services be defined', () => {
@@ -109,95 +121,8 @@ describe('Get All Orders UseCase', () => {
     expect(orderRepo).toBeDefined();
     expect(org_id).toBeDefined();
     expect(user_id).toBeDefined();
+    expect(user_id2).toBeDefined();
     expect(orgService).toBeDefined();
     expect(orgRepo).toBeDefined();
-  });
-
-  it('Should get all orders with 25 orders in the first page if the page parameter is not providede', async () => {
-    // Act
-    const orders = await getAllOrdersUseCase.execute({
-      org_id,
-    });
-
-    // Assert
-    expect(orders.has_next).toBeTruthy();
-    expect(orders.orders.length).toBe(25);
-    expect(orders.orders[0]).toBeInstanceOf(Order);
-  });
-
-  it('Should get all orders with 25 orders in the first page with the page parameter', async () => {
-    // Act
-    const orders = await getAllOrdersUseCase.execute({
-      org_id,
-      page: 0,
-    });
-
-    // Assert
-    expect(orders.has_next).toBeTruthy();
-    expect(orders.orders.length).toBe(25);
-    expect(orders.orders[0]).toBeInstanceOf(Order);
-  });
-
-  it('Should get all orders with 25 orders in the second page', async () => {
-    // Act
-    const orders = await getAllOrdersUseCase.execute({
-      org_id,
-    });
-    const orders2 = await getAllOrdersUseCase.execute({
-      org_id,
-      page: 1,
-    });
-
-    // Assert
-    expect(orders2.has_next).toBeTruthy();
-    expect(orders2.orders.length).toBe(25);
-    expect(orders2.orders[0]).toBeInstanceOf(Order);
-    expect(orders.orders[0].id).not.toBe(orders2.orders[0].id);
-    expect(orders.orders[1].id).not.toBe(orders2.orders[1].id);
-    expect(orders.orders[2].id).not.toBe(orders2.orders[2].id);
-    expect(orders.orders[3].id).not.toBe(orders2.orders[3].id);
-  });
-
-  it('Should get all orders with 18 orders in the third page', async () => {
-    // Act
-    const orders2 = await getAllOrdersUseCase.execute({
-      org_id,
-      page: 1,
-    });
-    const orders3 = await getAllOrdersUseCase.execute({
-      org_id,
-      page: 2,
-    });
-
-    // Assert
-    expect(orders3.has_next).toBeFalsy();
-    expect(orders3.orders.length).toBe(17);
-    expect(orders3.orders[0]).toBeInstanceOf(Order);
-    expect(orders2.orders[0].id).not.toBe(orders3.orders[0].id);
-    expect(orders2.orders[1].id).not.toBe(orders3.orders[1].id);
-    expect(orders2.orders[2].id).not.toBe(orders3.orders[2].id);
-    expect(orders2.orders[3].id).not.toBe(orders3.orders[3].id);
-  });
-
-  it('Should return 0 orders in the Fourth page', async () => {
-    // Act
-    const orders = await getAllOrdersUseCase.execute({
-      org_id,
-      page: 3,
-    });
-
-    // Assert
-    expect(orders.has_next).toBeFalsy();
-    expect(orders.orders.length).toBe(0);
-    expect(orders.orders[0]).toBeUndefined();
-  });
-
-  it('Should throw an error if the org does not exists', async () => {
-    // Asser
-    await expect(
-      getAllOrdersUseCase.execute({
-        org_id: 'org_id',
-      }),
-    ).rejects.toThrow(NotFoundException);
   });
 });
--- src/modules/order/usecases/GetAllOrdersUseCase.ts
+++ src/modules/order/usecases/GetAllOrdersUseCase.ts
@@ -1,7 +1,8 @@
-import { Inject, Injectable } from '@nestjs/common';
+import { Inject, Injectable, NotFoundException } from '@nestjs/common';
 import { IOrderContract } from 'src/core/application/contracts/order/IOrderContract';
+import { IOrganizationContract } from 'src/core/application/contracts/organization/IOrganizationContract';
 import { Order } from 'src/core/domain/entities/order';
-import { IORDER_CONTRACT } from 'src/shared/constants';
+import { IORDER_CONTRACT, IORGANIZATION_CONTRACT } from 'src/shared/constants';
 
 interface IGetAllOrdersOfOrgUseCase {
   execute(params: { org_id: string; page?: number }): Promise<{
@@ -15,12 +16,22 @@ export class GetAllOrdersOfOrgUseCase implements IGetAllOrdersOfOrgUseCase {
   constructor(
     @Inject(IORDER_CONTRACT)
     private readonly orderContract: IOrderContract,
+    @Inject(IORGANIZATION_CONTRACT)
+    private readonly orgContract: IOrganizationContract,
   ) {}
 
   async execute(params: { org_id: string; page?: number }): Promise<{
     has_next: boolean;
     orders: Order[];
   }> {
-    throw new Error('Method not implemented');
+    const orgExists = await this.orgContract.get({ id: params.org_id });
+
+    if (!orgExists) {
+      throw new NotFoundException('Org not found');
+    }
+
+    const orders = await this.orderContract.getAllOrders(params);
+
+    return orders;
   }
 }
