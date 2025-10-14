5144b32f3b5a4672c4f508fde73aca064fd9dee0
rafaapcode <rafaap2003@gmail.com>
2025-10-14 09:55:29 -0300

    fix: fixing error of import on the product module

--- src/modules/product/product.module.ts
+++ src/modules/product/product.module.ts
@@ -7,17 +7,35 @@ import {
   IPRODUCT_CONTRACT,
 } from 'src/shared/constants';
 import { CategoryService } from '../category/category.service';
+import { CategoryRepository } from '../category/repo/category.repository';
 import { IngredientService } from '../ingredient/ingredient.service';
+import { IngredientRepository } from '../ingredient/repo/ingredient.repository';
 import { OrganizationService } from '../organization/organization.service';
+import { OrganizationRepo } from '../organization/repo/organization.repo';
 import { ProductController } from './product.controller';
 import { ProductService } from './product.service';
 import { ProductRepository } from './repo/product.repository';
+import { CreateProductUseCase } from './usecases/CreateProductUseCase';
+import { DeleteProductUseCase } from './usecases/DeleteProductUseCase';
+import { GetAllProductUseCase } from './usecases/GetAllProductsUseCase';
+import { GetProductByCategoryUseCase } from './usecases/GetProductByCategoryUseCase';
+import { GetProductUseCase } from './usecases/GetProductUseCase';
+import { UpdateProductUseCase } from './usecases/UpdateProductUseCase';
 
 @Module({
   imports: [DatabaseModule],
   controllers: [ProductController],
   providers: [
     ProductRepository,
+    CategoryRepository,
+    IngredientRepository,
+    OrganizationRepo,
+    CreateProductUseCase,
+    UpdateProductUseCase,
+    DeleteProductUseCase,
+    GetAllProductUseCase,
+    GetProductByCategoryUseCase,
+    GetProductUseCase,
     {
       provide: IPRODUCT_CONTRACT,
       useClass: ProductService,
