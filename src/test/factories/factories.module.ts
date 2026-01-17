a958f6e02d0baef458fca693def340b4478ce0ab
Rafael Aparecido <rafaap2003@gmail.com>
2026-01-17 16:48:43 -0300

    tests: creating the factories moduel

--- src/test/factories/factories.module.ts
+++ src/test/factories/factories.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
+import { DatabaseModule } from 'src/infra/database/database.module';
 import { FactoriesService } from './factories.service';
 
 @Module({
+  imports: [DatabaseModule],
   providers: [FactoriesService],
   exports: [FactoriesService],
 })
--- src/test/factories/factories.service.ts
+++ src/test/factories/factories.service.ts
@@ -1,4 +1,42 @@
+import { faker } from '@faker-js/faker';
 import { Injectable } from '@nestjs/common';
+import { UserRole } from 'src/core/domain/entities/user';
+import { PrismaService } from 'src/infra/database/database.service';
 
 @Injectable()
-export class FactoriesService {}
+export class FactoriesService {
+  constructor(private readonly prismaService: PrismaService) {}
+
+  async generateUserInfo(role: UserRole, persistent?: boolean) {
+    const userName = faker.person.fullName();
+    const userId = faker.string.uuid();
+    const userEmail = faker.internet.email();
+    const userCpf = faker.string.numeric(11);
+    const hashBcrypt = faker.string.alphanumeric(60);
+
+    if (persistent) {
+      const user = await this.prismaService.user.create({
+        data: {
+          name: userName,
+          email: userEmail,
+          cpf: userCpf,
+          password: hashBcrypt,
+          role,
+        },
+      });
+
+      return user;
+    }
+
+    return {
+      name: userName,
+      id: userId,
+      email: userEmail,
+      cpf: userCpf,
+      password: hashBcrypt,
+      role,
+      created_at: new Date(),
+      updated_at: new Date(),
+    };
+  }
+}
